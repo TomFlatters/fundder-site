1f017aea1145962db0047cd62a5417357cbb8eb4
adrikoz <adrikozh@gmail.com>
2020-09-06 14:55:34 +0100

    changed mainpanel

--- src/components/mainpanel.js
+++ src/components/mainpanel.js
@@ -4,21 +4,29 @@ import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../App.css';
-import phone from '../assets/phone.png';
+import phone from '../assets/accordion-view.png';
+import iosButton from '../assets/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.png';
+import googleButton from '../assets/google-play-badge.png';
+import MobileStoreButton from 'react-mobile-store-button';
 
 const MainPanel = () => {
+  const iOSUrl = 'https://itunes.apple.com/us/app/all-of-the-lights/id959389722?mt=8';
   return (
-    <div id="home" className="px-0 mb-5 mx-0 py-panel">
+    
+    <div id="home" className="px-0 mb-0 mx-0 py-5 px-5">
       <Row className="flex-row space-between align-start">
-        <Col xs={12} md={12} lg={6} xl={7} xxl={7} id="fundder-title-block" className="flex-col order-lg-2 text-right">
-          <h1 className="fundder-title mt-lg-4 py-lg-4">Our mission is to get people hooked on giving.</h1>
+        <Col xs={12} md={6} lg={5} xl={5} xxl={5} id="fundder-title-block" className="flex-col offset-lg-1 order-lg-1 text-left">
+          <h1 className="fundder-title mt-lg-4 pt-lg-4">Our mission is to get people hooked on giving.</h1>
           <p className="fundder-body">Fundder is a social network for better, easier fundraising.</p>
-          <div className="mt-lg-4">
-            <button class="fundder-button">Coming Soon</button>
-          </div>
+          <a href="https://apps.apple.com/gb/app/fundder/id1529120882" target="_blank">
+            <img className="store-button pt-5" src={iosButton}/>
+          </a>
+          <a href="https://play.google.com/store/apps/details?id=com.fundder&hl=en_US" target="_blank">
+            <img className="store-button py-4" src={googleButton}/>
+          </a>
         </Col>
-        <Col xs={12} md={12} lg={3} xl={4} xxl={4} className="flex-col order-lg-1">
-          <div className="my-4">
+        <Col xs={12} md={6} lg={5} xl={5} xxl={5} className="flex-col order-lg-2">
+          <div className="my-0">
             <img src={phone} className="demo-phone" />
           </div>
         </Col>
